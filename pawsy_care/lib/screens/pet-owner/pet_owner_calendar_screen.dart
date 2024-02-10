848962716727bd1d525145ade2f0359ad8647939
Marko Arsov <mar@mca.dev>
2024-02-10 22:07:47 +0100

    Add syncfusion_flutter_calendar dependency and implement calendar screen

--- pawsy_care/lib/screens/pet-owner/pet_owner_calendar_screen.dart
+++ pawsy_care/lib/screens/pet-owner/pet_owner_calendar_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pawsy_care/data-access/firestore.dart';
 import 'package:pawsy_care/models/booking.dart';
-import 'package:pawsy_care/widgets/bookings/booking_card_widget.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class PetOwnerCalendarScreen extends StatefulWidget {
   const PetOwnerCalendarScreen({super.key});
@@ -32,11 +32,14 @@ class PetOwnerCalendarScreenState extends State<PetOwnerCalendarScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        // TODO implement calendar
-        children: bookings
-            .map((booking) => BookingCardWidget(booking: booking))
-            .toList(),
+      body: SfCalendar(
+        view: CalendarView.month,
+        dataSource: _getCalendarData(),
+        onTap: (CalendarTapDetails details) {
+          if (details.targetElement == CalendarElement.calendarCell) {
+            _handleDateTap(context, details.date!);
+          }
+        },
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: 2,
@@ -64,5 +67,64 @@ class PetOwnerCalendarScreenState extends State<PetOwnerCalendarScreen> {
         },
       ),
     );
+  }
+
+  _DataSource _getCalendarData() {
+    List<Appointment> appointments = [];
+
+    for (var booking in bookings) {
+      appointments.add(Appointment(
+        startTime: booking.date,
+        endTime: booking.date.add(const Duration(hours: 2)),
+        subject: booking.service.name,
+      ));
+    }
+
+    return _DataSource(appointments);
+  }
+
+  void _handleDateTap(BuildContext context, DateTime selectedDate) {
+    List<Booking> selectedBookings = bookings
+        .where((booking) =>
+            booking.date.year == selectedDate.year &&
+            booking.date.month == selectedDate.month &&
+            booking.date.day == selectedDate.day)
+        .toList();
+
+    if (selectedBookings.isNotEmpty) {
+      _showBookingsDialog(context, selectedDate, selectedBookings);
+    }
+  }
+
+  void _showBookingsDialog(
+      BuildContext context, DateTime selectedDate, List<Booking> bookings) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Bookings on ${selectedDate.toLocal()}'),
+          content: Column(
+            children: bookings
+                .map((booking) => Text(
+                    '${booking.service.name} - ${booking.date.hour}:${booking.date..minute}'))
+                .toList(),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class _DataSource extends CalendarDataSource {
+  _DataSource(List<Appointment> source) {
+    appointments = source;
   }
 }
