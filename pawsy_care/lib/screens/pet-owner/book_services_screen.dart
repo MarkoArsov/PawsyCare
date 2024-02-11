44dc1896e8649dc9e0e3945c31cbff7f0487d5d3
Marko Arsov <mar@mca.dev>
2024-02-11 18:58:04 +0100

    Add Firestore integration and load services and pets

--- pawsy_care/lib/screens/pet-owner/book_services_screen.dart
+++ pawsy_care/lib/screens/pet-owner/book_services_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:pawsy_care/models/booking.dart';
+import 'package:pawsy_care/data-access/firestore.dart';
+import 'package:pawsy_care/models/pet.dart';
 import 'package:pawsy_care/models/service.dart';
 import 'package:pawsy_care/widgets/services/service_card.widget.dart';
 
@@ -11,9 +12,33 @@ class BookServicesScreen extends StatefulWidget {
 }
 
 class BookServicesScreenState extends State<BookServicesScreen> {
+  final FirestoreService _firestoreService = FirestoreService();
+
   final List<Service> services = [];
 
-  final List<Booking> bookings = [];
+  final List<Pet> pets = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadServices();
+    _loadPets();
+  }
+
+  Future<void> _loadServices() async {
+    List<Service> userServices = await _firestoreService.getAllServices();
+    setState(() {
+      services.addAll(userServices);
+    });
+  }
+
+  Future<void> _loadPets() async {
+    String userId = _firestoreService.getCurrentUserID();
+    List<Pet> userPets = await _firestoreService.getPetsByUser(userId);
+    setState(() {
+      pets.addAll(userPets);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
